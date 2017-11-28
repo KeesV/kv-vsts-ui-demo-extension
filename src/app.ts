e84df7294d49f8a95b4c996447519e766384c944
Kees Verhaar <kverhaar@gmail.com>
2017-11-28 13:01:53 +0100

    Initial Basic Structure

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+/// <reference types="vss-web-extension-sdk" />
+
+import * as WitExtensionContracts  from "TFS/WorkItemTracking/ExtensionContracts";
+
+export function register() {
+    writeOutput();
+    
+    VSS.register(VSS.getContribution().id, {
+        onLoaded: (args: WitExtensionContracts.IWorkItemLoadedArgs) => {
+            // Stuff to do when item is loaded here
+            writeOutput();
+        },
+        onUnloaded: (args: WitExtensionContracts.IWorkItemChangedArgs) => {
+            // Stuff to do when item is unloaded here
+        }
+        ,
+        onRefreshed: (args: WitExtensionContracts.IWorkItemChangedArgs) => {
+            // Stuff to do when item is refreshed here
+        },
+        onSaved: (args: WitExtensionContracts.IWorkItemChangedArgs) => {
+            // Stuff to do when item is saved here
+        }
+    } as WitExtensionContracts.IWorkItemNotificationListener)
+}
+
+function writeOutput() {
+
+    let text: string = "Hello World!";
+    document.write(text);
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: "./src/app.ts"
+  },
+  output: {
+    filename: "app.js",
+    libraryTarget: "amd"
+  },
+  externals: [
+    /^VSS\/.*/, /^TFS\/.*/, /^q$/
+  ],
+  module: {
+    rules: [
+    {
+      test: /\.tsx?$/,
+      loader: "ts-loader"
+    }]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.css', '.ts']
+  },
+  devtool: 'source-map',
+  devServer: {
+    https: true
+  }
+};
